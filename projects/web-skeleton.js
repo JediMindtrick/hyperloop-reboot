e6de65fbd8e3dbc35508232f3b0b7463f927eb5b
JediMindtrick <brandonjwilhite@gmail.com>
2015-01-08 07:47:31 -0500

    live reload added to web-skeleton

--- projects/web-skeleton.js
+++ projects/web-skeleton.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
-var _cfgFile = path.join(process.env.CONFIG_LOCATION,'box_config.json');
+var http = require('http');
+reload = require('reload');
+var bodyParser = require('body-parser');
 
+var _cfgFile = path.join(process.env.CONFIG_LOCATION,'box_config.json');
 var cfg = JSON.parse(fs.readFileSync(_cfgFile, 'utf8'));
 
 var app = express();
+//middleware
+app.set('views', path.join(__dirname, 'spa'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
+app.use(express.static(path.join(__dirname, 'spa')));
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', function(req, res){
-    res.send('Hello from inside a container!');
+//routes
+app.get('/', function(req, res) {
+  res.render('index.html');
 });
 
-app.get('/foo',function(req,res){
-  res.send('bar');
-});
+//start
+var server = app.listen(cfg.webPort);
 
-app.listen(cfg.webPort);
+//add live reload functionality
+reload.all(server,app);
 console.log('listening on ' + cfg.webPort);
